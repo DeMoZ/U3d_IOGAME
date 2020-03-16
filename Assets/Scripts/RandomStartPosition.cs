727b173ba87263d575876bf21d001eb0aa4ae1d8
demozbucket <demozbox@gmail.com>
2020-03-16 22:57:16 +0300

    imported Mirror framework

---
+++ Assets/Scripts/RandomStartPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStartPosition : MonoBehaviour
+{
+    private Transform _transform;
+    void Start()
+    {
+        _transform = transform;
+        _transform.position = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)); 
+    }
+
+    
+}
