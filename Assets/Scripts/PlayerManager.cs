59e630cbd6b579e8ddb0c6c4284dc7a809f543bf
demozbucket <demozbox@gmail.com>
2020-03-16 23:40:01 +0300

    Creating simple input/move

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,16 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : NetworkBehaviour
+{
+    [SerializeField] SimpleKeyboardInput _input;
+    [SerializeField] SimpleMove _move;
+
+    private void Update()
+    {
+        //if (isLocalPlayer)
+            _move.Move(_input.GetInput());
+    }
+}
---
+++ Assets/Scripts/SimpleKeyboardInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleKeyboardInput : MonoBehaviour, IInput
+{
+    private Vector3 direction = Vector3.zero;
+    public Vector3 GetInput()
+    {
+        return direction;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        direction = Vector3.ClampMagnitude(direction, 1);
+    }
+}
