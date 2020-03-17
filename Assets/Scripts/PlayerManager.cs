43fe811ae99c6b767359dd02f30a2fb5d05c4383
demozbucket <demozbox@gmail.com>
2020-03-17 23:28:48 +0300

    Simple Movement

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 
 public class PlayerManager : NetworkBehaviour
 {
-    [SerializeField] SimpleKeyboardInput _input;
-    [SerializeField] SimpleMove _move;
+    [SerializeField] IInput _input;
+    [SerializeField] IMove _move;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _input = GetComponent<IInput>();
+        _move = GetComponent<IMove>();
+    }
 
     private void Update()
-    {
+    {        
         //if (isLocalPlayer)
+        //{
+            Vector3 dirtVelocity = _input.GetInput();
+            Vector3 velocity = Vector3.zero;
+            velocity.x = dirtVelocity.x;
+            velocity.z = dirtVelocity.y;
+
             _move.Move(_input.GetInput());
+       //}
+
     }
 }
