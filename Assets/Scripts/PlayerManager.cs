fbe8afa1e940c4de7af3222162d1c4e02f08e059
Roman Kolychev <kolychevra@sibintek.ru>
2020-03-20 00:02:35 +0300

    Player movement related to camera

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,12 +1,16 @@
-﻿using Mirror;
-using System.Collections;
-using System.Collections.Generic;
+﻿//#define INSTALL_CAMERA
+using Mirror;
 using UnityEngine;
 
 public class PlayerManager : NetworkBehaviour
 {
-    [SerializeField] IInput _input;
-    [SerializeField] IMove _move;
+#if INSTALL_CAMERA
+    [SerializeField] GameObject _playerCameraPrefab;
+#endif
+    Transform _cameraT;
+
+    IInput _input;
+    IMove _move;
 
     private void Start()
     {
@@ -17,19 +21,49 @@ public class PlayerManager : NetworkBehaviour
     {
         _input = GetComponent<IInput>();
         _move = GetComponent<IMove>();
+
+#if INSTALL_CAMERA
+        if (!_playerCameraPrefab)
+            throw new System.Exception($"_ Camera prefab not set for {this}");
+
+        // instantiate player camera
+        Vector3 position = Vector3.zero;
+        Quaternion rotation = Quaternion.LookRotation(transform.position - position, Vector3.up);
+        GameObject cameraGO = Instantiate(_playerCameraPrefab, position, rotation);
+
+        _cameraT = _playerCameraPrefab.transform;
+#else
+          _cameraT = Camera.main.transform;
+#endif
     }
 
     private void Update()
-    {        
-       if (isLocalPlayer)
-       {
+    {
+        if (isLocalPlayer)
+        {
             Vector3 dirtVelocity = _input.GetInput();
             Vector3 velocity = Vector3.zero;
             velocity.x = dirtVelocity.x;
             velocity.z = dirtVelocity.y;
 
-            _move.Move(_input.GetInput());
-       }
+            // camera forward and right vectors:
+            Vector3 forward = _cameraT.forward;
+            Vector3 right = _cameraT.right;
+
+            //project forward and right vectors on the horizontal plane (y = 0)
+            forward.y = 0f;
+            right.y = 0f;
+            forward.Normalize();
+            right.Normalize();
+
+            //this is the direction in the world space we want to move:
+            velocity = forward * velocity.z + right * velocity.x;
+
+            // velocity magnitude not more than 1
+            velocity = Vector3.ClampMagnitude(velocity, 1);
+
+            _move.Move(velocity);
+        }
 
     }
 }
