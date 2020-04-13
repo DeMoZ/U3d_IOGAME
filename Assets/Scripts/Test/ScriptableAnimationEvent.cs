cb9405886a20edabe5bf76a18022212ab200f079
Roman Kolychev <demobox@gmail.com>
2020-04-14 00:06:42 +0300

    Test scripts for scriptable object in animation events created.

---
+++ Assets/Scripts/Test/ScriptableAnimationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    [CreateAssetMenu(fileName = "AnimationEventScriptable", menuName = "ScriptableObjects/AnimationEvents")]
+    public class ScriptableAnimationEvent : ScriptableObject
+    {
+        public TheAttack.AttackStates AttackStateValue;
+        public int IntValue;
+        public float FloatValue;
+
+        public override string ToString()
+        {
+            string rezult = "";
+            rezult+= $"_";
+            rezult += $"AttackStateValue ={AttackStateValue} ; ";
+            rezult += $"IntValue ={IntValue} ; ";
+            rezult += $"FloatValue ={FloatValue} ; ";
+
+            return rezult;
+        }
+    }
+}
---
+++ Assets/Scripts/Test/ScriptableEventListenner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class ScriptableEventListenner : MonoBehaviour
+    {
+        public void ScriptableAttackTest(ScriptableAnimationEvent scriptableValues)
+        {
+            Debug.Log(scriptableValues.ToString());
+        }
+    }
+}
